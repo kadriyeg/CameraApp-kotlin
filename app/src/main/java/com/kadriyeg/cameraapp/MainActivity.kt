512ebf502c9a2d94cc54810ce8d967841a588201
kadriyeg <denizk2407@gmail.com>
2022-12-16 14:11:06 +0300

    Constants object has been created

--- app/src/main/java/com/kadriyeg/cameraapp/MainActivity.kt
+++ app/src/main/java/com/kadriyeg/cameraapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kadriyeg.cameraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kadriyeg.cameraapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
